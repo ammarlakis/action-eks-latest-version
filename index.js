55356daaf9dda7154fd482ad189e7cc582def358
Ammar Lakis <ammar.lakis@gmail.com>
2024-04-28 09:27:54 +0200

    Fetch eks latest version

---
+++ index.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const core = require("@actions/core");
+
+async function fetchEKSVersion() {
+  try {
+    const response = await axios.get(
+      "https://docs.aws.amazon.com/eks/latest/userguide/platform-versions.html"
+    );
+    const $ = cheerio.load(response.data);
+    const versionText = $("div#main-col-body>h2")
+      .first()
+      .attr("id")
+      .match(/(?<version>\d+\.\d+)/).groups["version"];
+
+    console.log(`Latest EKS Version: ${versionText}`);
+    core.setOutput("eks-version", versionText);
+  } catch (error) {
+    console.error(`Failed to fetch EKS version: ${error}`);
+    core.setFailed(`Action failed with error: ${error}`);
+  }
+}
+
+fetchEKSVersion();
